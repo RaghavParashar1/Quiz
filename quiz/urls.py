32e74bbdcf33b091b0f3290c5e7685e1fdd6d58d
RaghavParashar1 <rparasharr@gmail.com>
2021-02-11 21:00:52 +0530

    Initial Commit

--- quiz/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-import quiz.views as quiz_views
-
-urlpatterns = [
-	url(r'^$', quiz_views.qpage),
-	
-]
--- quizapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from quiz.models import Quiz
-
-class QuizApiSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model = Quiz
-		fields = '__all__'
